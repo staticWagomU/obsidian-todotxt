fd24e93f203be80215a83f4bea325394c6e078f5
staticWagomU <to6wa17@gmail.com>
2026-01-08 17:08:05 +0900

    refactor: Subtask 1 - add JSDoc parameters to filterByPriority
    - @param/@returns アノテーション追加でドキュメント強化

--- src/lib/filter.ts
+++ src/lib/filter.ts
@@ -3,6 +3,9 @@ import type { Todo } from "./todo";
 /**
  * Filter todos by priority
  * Returns only tasks that match the specified priority
+ * @param todos - Array of todos to filter
+ * @param priority - Priority value to filter by (A-Z)
+ * @returns Filtered array of todos matching the priority
  */
 export function filterByPriority(todos: Todo[], priority: string): Todo[] {
 	return todos.filter(todo => todo.priority === priority);

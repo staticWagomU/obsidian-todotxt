5252446fb531aefb38548686c7a1d526e5cc281c
staticWagomU <to6wa17@gmail.com>
2026-01-08 20:17:17 +0900

    refactor: Subtask 1 - JSDoc詳細化 (型安全性)
    Refactorチェックリスト:
    - [改善不要] コード重複: 重複なし
    - [改善実施] 型安全性: JSDocに@param/@returns/@remarksを追加、動作仕様を明示
    - [改善不要] 関数分割: 関数が単純すぎて分割不要
    - [改善不要] 命名明確性: groupByProject/todosは明確

--- src/lib/group.ts
+++ src/lib/group.ts
@@ -6,7 +6,14 @@ import type { Todo } from "./todo";
 
 /**
  * Group todos by project tags (+project)
- * Returns a Map where keys are project names and values are arrays of todos
+ *
+ * @param todos - Array of todos to group
+ * @returns Map where keys are project names and values are arrays of todos containing that project
+ * @remarks
+ * - Empty input returns empty Map
+ * - Todos without projects will be included in "未分類" group (to be implemented)
+ * - Todos with multiple projects will appear in all corresponding groups (to be implemented)
+ * - Order within each group is preserved from input array
  */
 export function groupByProject(todos: Todo[]): Map<string, Todo[]> {
 	return new Map();
